769205cd9f4b9b977a82c04ac33fe8ef9ace3b64
Lucas Silva <luckkass89@gmail.com>
2021-08-25 00:59:35 -0300

    EventHubsSender app finalizado

---
+++ EventHub/EventHubsQuickStart/EventHubsSender/EventHubSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventHubsSender
+{
+    public class EventHubSettings
+    {
+        public string ConnectionString { get; set; }
+        public string Namespace { get; set; }
+        public string EventHubName { get; set; }
+        public int NumOfEvents { get; set; }
+    }
+}
---
+++ EventHub/EventHubsQuickStart/EventHubsSender/Program.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubsSender
+{
+    class Program
+    {
+        static EventHubProducerClient producerClient;
+
+        static async Task Main(string[] args)
+        {
+            EventHubSettings hubSettings = GetEventHubSettings();
+
+            producerClient = new EventHubProducerClient(hubSettings.ConnectionString, hubSettings.EventHubName);
+
+            using (var eventBatch = await producerClient.CreateBatchAsync())
+            {
+                for (int i = 1; i <= hubSettings.NumOfEvents; i++)
+                {
+                    var eventBody = Encoding.UTF8.GetBytes($"Event {1}");
+                    var eventData = new EventData(eventBody);
+
+                    if (!eventBatch.TryAdd(eventData))
+                        throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+                }
+
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine($"A batch of {hubSettings.NumOfEvents} events has been published.");
+            }
+        }
+
+        private static EventHubSettings GetEventHubSettings() 
+            => GetConfiguration()
+                .GetSection(nameof(EventHubSettings))
+                .Get<EventHubSettings>();
+
+        private static IConfigurationRoot GetConfiguration() 
+            => new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json", false)
+                            .Build();
+    }
+}
