eef7978259781c13b1e5c64a21caa0b9b2c5173b
Lucas Silva <luckkass89@gmail.com>
2021-08-28 14:43:09 -0300

    Notification hub - Enterprise push architectural

---
+++ NotificationHub/EnterprisePushArchitecture/EnterprisePushBackendSystem/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Threading;
+
+namespace EnterprisePushBackendSystem
+{
+    class Program
+    {
+        static string sampleTopic, connectionString;
+
+        static void Main(string[] args)
+        {
+            sampleTopic = "notifications";
+            connectionString = CloudConfigurationManager
+                .GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            CreateTopic();
+            SendMessage();
+        }
+
+        private static void CreateTopic()
+        {
+            var namespaceManager = NamespaceManager
+                .CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.TopicExists(sampleTopic))
+            {
+                namespaceManager.CreateTopic(sampleTopic);
+            }
+        }
+
+        private static void SendMessage()
+        {
+            var client = TopicClient
+                .CreateFromConnectionString(connectionString, sampleTopic);
+
+            var messages = new string[]
+            {
+                "Employee Id '{0}' has joined.",
+                "Employee Id '{0}' has left.",
+                "Employee Id '{0}' has switched to a different team."
+            };
+
+            while (true)
+            {
+                var random = new Random();
+                var employeeId = random.Next(10000, 99999).ToString();
+                var randomMessage = messages[random.Next(0, messages.Length)];
+                var notification = string.Format(randomMessage, employeeId);
+
+                var message = new BrokeredMessage(notification);
+                client.Send(message);
+
+                Console.WriteLine("{0} Message sent - '{1}'", DateTime.Now, notification);
+
+                Thread.Sleep(new TimeSpan(0, 0, 10));
+            }
+        }
+    }
+}
---
+++ NotificationHub/EnterprisePushArchitecture/ReceiveAndSendNotification/Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Azure;
+using Microsoft.Azure.NotificationHubs;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiveAndSendNotification
+{
+    class Program
+    {
+        static string sampleTopic, sampleSubscription, connectionString;
+        static NotificationHubClient hub;
+
+        static void Main(string[] args)
+        {
+            connectionString = CloudConfigurationManager
+                .GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            CreateSubscription(connectionString);
+
+            ReceiveMessageAndSendNotification(connectionString);
+        }
+
+        private static void CreateSubscription(string connectionString)
+        {
+            sampleTopic = "notifications";
+            sampleSubscription = "webjobsubscription";
+            var namespaceManager = Microsoft.ServiceBus.NamespaceManager
+                .CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.SubscriptionExists(sampleTopic, sampleSubscription))
+            {
+                namespaceManager.CreateSubscription(sampleTopic, sampleSubscription);
+            }
+        }
+
+        private static void ReceiveMessageAndSendNotification(string connectionString)
+        {
+            var hubConnectionString = CloudConfigurationManager
+                .GetSetting("Microsoft.NotificationHub.ConnectionString");
+
+            hub = NotificationHubClient
+                .CreateClientFromConnectionString(hubConnectionString, "enterprisepushservicehub");
+
+            var client = SubscriptionClient
+                .CreateFromConnectionString(connectionString, sampleTopic, sampleSubscription);
+
+            client.Receive();
+
+            while (true)
+            {
+                var message = client.Receive();
+                var toastMessage = @"<toast><visual><binding template=""ToastText01""><text id=""1"">{messagepayload}</text></binding></visual></toast>";
+
+                if (message != null)
+                {
+                    try
+                    {
+                        Console.WriteLine(message.MessageId);
+                        Console.WriteLine(message.SequenceNumber);
+                        var messageBody = message.GetBody<string>();
+                        Console.WriteLine("Body: " + messageBody + "\n");
+
+                        toastMessage = toastMessage.Replace("{messagepayload}", messageBody);
+                        SendNotificationAsync(toastMessage);
+
+                        message.Complete();
+                    }
+                    catch (Exception)
+                    {
+                        message.Abandon();
+                    }
+                }
+            }
+        }
+
+        private static async void SendNotificationAsync(string toastMessage)
+        {
+            await hub.SendWindowsNativeNotificationAsync(toastMessage);
+        }
+    }
+}
