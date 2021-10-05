935dcf5ed884e87bf1cba02968d27602bca2f0a8
Lucas Silva <luckkass89@gmail.com>
2021-10-04 22:17:45 -0300

    Quickstart Azure Cache for Redis

---
+++ Redis/RedisQuickstart/Controllers/CacheController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Collections.Generic;
+
+namespace RedisQuickstart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IDatabase cache;
+
+        public CacheController(IDatabase cache) 
+            => this.cache = cache;
+
+        [HttpGet("{key}")]
+        public string Get(string key) 
+            => cache.StringGet(key);
+
+        [HttpPost]
+        public void Post([FromBody] KeyValuePair<string, string> keyValue) 
+            => cache.StringSet(keyValue.Key, keyValue.Value);
+    }
+}
